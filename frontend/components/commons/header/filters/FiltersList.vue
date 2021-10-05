9101ced5ba2d9711c845329e758f609396bc17ca
LeireA <leire@recogn.ai>
2021-10-05 10:42:52 +0200

    rename sort to Sort group

--- frontend/components/commons/header/filters/FiltersList.vue
+++ frontend/components/commons/header/filters/FiltersList.vue
@@ -25,7 +25,7 @@
           }"
           @click="selectGroup(group)"
         >
-          <svgicon v-if="group === 'sort'" name="sort" width="14" height="14" />
+          <svgicon v-if="group === 'Sort'" name="sort" width="14" height="14" />
           {{ group }}
           <span v-if="itemsAppliedOnGroup(group)"
             >({{ itemsAppliedOnGroup(group) }})</span
